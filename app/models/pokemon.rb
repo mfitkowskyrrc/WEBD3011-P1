9c4111621a37955154d191c27106b36227829912
mfitkowskyrrc <mfitkowsky@academic.rrc.ca>
2025-03-28 20:49:44 -0500

    added validation for data sources

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,3 +1,6 @@
 class Pokemon < ApplicationRecord
   belongs_to :trainer
+
+  validates :name, presence: true
+  validates :dex_number, presence: true
 end
--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,3 +1,4 @@
 class Trainer < ApplicationRecord
   has_many :pokemon, dependent: :destroy
+  validates :name, presence: true
 end
