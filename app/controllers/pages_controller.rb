282cee9192c423de37e73966f5ff4f24a52335aa
mfitkowskyrrc <mfitkowsky@academic.rrc.ca>
2025-03-29 11:58:30 -0500

    created page and routes for displaying pokemon by type

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def about
   end
+  def pokemon_by_type
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   root to: 'pages#about'
 
+  get 'pokemon_by_type', to: 'pages#pokemon_by_type'
+
   get "up" => "rails/health#show", as: :rails_health_check
 
 end
